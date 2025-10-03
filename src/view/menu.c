d3633a25d51aa56c672bf6ded5b3bf3ad73c9b6e
cauasturaro <caua.sturaro111@gmail.com>
2025-10-03 12:16:02 -0300

    feat: menu + alterações em makefile

---
+++ src/view/menu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "menu.h"
+#include "../utils/utils.h"
+
+void show_menu()
+{
+    clear_screen();
+    printf("\n--- MENU PRINCIPAL ---\n");
+    printf("1 - Cadastrar estudante\n");
+    printf("2 - Cadastrar professor\n");
+    printf("3 - Listar estudantes\n");
+    printf("4 - Listar professores\n");
+    printf("0 - Sair\n");
+    printf("Escolha uma opcao: ");
+}
